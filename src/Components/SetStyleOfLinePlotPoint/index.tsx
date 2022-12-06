3cdb5fb3a93c1441f360fd18879054c7d77b7e6c
valdisneinilo <valdisneinilo@gmail.com>
2022-12-06 10:24:47 -0300

    refatoração ajustes layout do componente

--- src/Components/SetStyleOfLinePlotPoint/index.tsx
+++ src/Components/SetStyleOfLinePlotPoint/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Line } from "@ant-design/plots";
 import { Props } from "./types";
+import { ContainerTooltip } from "./styles";
 
 const SetStyleOfLinePlotPoint: React.FC<Props> = ({ data }) => {
   const config = {
@@ -17,9 +18,7 @@ const SetStyleOfLinePlotPoint: React.FC<Props> = ({ data }) => {
         lineWidth: 2,
       },
     },
-    tooltip: {
-      showMarkers: false,
-    },
+
     state: {
       active: {
         style: {
@@ -34,6 +33,39 @@ const SetStyleOfLinePlotPoint: React.FC<Props> = ({ data }) => {
         type: "marker-active",
       },
     ],
+    tooltip: {
+      showMarkers: false,
+      // fields: ["x", "y"],
+      customContent: (title: any, data: any) => {
+        const dataFormated = data[0]?.data;
+        const unidade = data[0]?.data?.unidade;
+        const classe = data[0]?.data?.unidade?.classe;
+        return (
+          <ContainerTooltip>
+            <h3>{dataFormated?.indicador}</h3>
+            <h3>{dataFormated?.ano}</h3>
+            <h3>
+              {unidade?.id} : {dataFormated?.valor}
+            </h3>
+            <div>
+              <sub>
+                Classe:{" "}
+                {classe === "N"
+                  ? "unidade numérica"
+                  : classe === "$"
+                  ? "unidade monetária"
+                  : classe === "P"
+                  ? "unidade proporcional"
+                  : "unidade genérica"}
+              </sub>
+            </div>
+            <div>
+              <sub>Multiplicador: {unidade?.multiplicador}</sub>
+            </div>
+          </ContainerTooltip>
+        );
+      },
+    },
   };
   return <Line {...config} />;
 };
--- src/Components/SetStyleOfLinePlotPoint/types.ts
+++ src/Components/SetStyleOfLinePlotPoint/types.ts
@@ -1,3 +1,16 @@
+interface dados {
+  type?: string;
+  sales?: number;
+  ano: string;
+  valor: number;
+  indicador: string;
+  unidade: {
+    id: string;
+    classe: string;
+    multiplicador: number;
+  };
+}
+
 export interface Props {
-  data: any;
+  data: dados[];
 }
