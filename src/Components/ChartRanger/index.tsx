276a905d141517257274ba60ea06ea6a5dd68cd7
valdisneinilo <valdisneinilo@gmail.com>
2022-12-06 10:08:29 -0300

    personalização das informações do gráfico chartRange

--- src/Components/ChartRanger/index.tsx
+++ src/Components/ChartRanger/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Column } from "@ant-design/plots";
 import { Props } from "./types";
+import { ContainerTooltip } from "./styles";
 
 const ChartRanger: React.FC<Props> = ({ data }) => {
   const config = {
@@ -15,6 +16,39 @@ const ChartRanger: React.FC<Props> = ({ data }) => {
     slider: {
       start: 0,
       end: 0.5,
+    },
+    tooltip: {
+      // fields: ["x", "y"],
+      customContent: (title: any, data: any) => {
+        const dataFormated = data[0]?.data;
+        const unidade = data[0]?.data?.unidade;
+        const classe = data[0]?.data?.unidade?.classe;
+        console.log(data);
+        return (
+          <ContainerTooltip>
+            <h3>{dataFormated?.indicador}</h3>
+            <h3>{dataFormated?.ano}</h3>
+            <h3>
+              {unidade?.id} : {dataFormated?.valor}
+            </h3>
+            <div>
+              <sub>
+                Classe:{" "}
+                {classe === "N"
+                  ? "unidade numérica"
+                  : classe === "$"
+                  ? "unidade monetária"
+                  : classe === "P"
+                  ? "unidade proporcional"
+                  : "unidade genérica"}
+              </sub>
+            </div>
+            <div>
+              <sub>Multiplicador: {unidade?.multiplicador}</sub>
+            </div>
+          </ContainerTooltip>
+        );
+      },
     },
   };
 
