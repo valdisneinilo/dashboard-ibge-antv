b2ace1f92d61669e2f59974d9573aea7360b154b
valdisneinilo <valdisneinilo@gmail.com>
2022-12-06 10:05:27 -0300

    personalização das informações do gráfico

--- src/Components/BasicColumnPlot/index.tsx
+++ src/Components/BasicColumnPlot/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Column } from "@ant-design/charts";
+import { Column, Datum } from "@ant-design/charts";
 import { Props } from "./types";
+import { ContainerTooltip } from "./styles";
 
 const BasicColumnPlot: React.FC<Props> = ({ data }) => {
   const config: any = {
@@ -11,6 +12,38 @@ const BasicColumnPlot: React.FC<Props> = ({ data }) => {
       style: {
         fill: "#FFFFFF",
         opacity: 0.6,
+      },
+    },
+    tooltip: {
+      // fields: ["x", "y"],
+      customContent: (title: any, data: any) => {
+        const dataFormated = data[0]?.data;
+        const unidade = data[0]?.data?.unidade;
+        const classe = data[0]?.data?.unidade?.classe;
+        return (
+          <ContainerTooltip>
+            <h3>{dataFormated?.indicador}</h3>
+            <h3>{dataFormated?.ano}</h3>
+            <h3>
+              {unidade?.id} : {dataFormated?.valor}
+            </h3>
+            <div>
+              <sub>
+                Classe:{" "}
+                {classe === "N"
+                  ? "unidade numérica"
+                  : classe === "$"
+                  ? "unidade monetária"
+                  : classe === "P"
+                  ? "unidade proporcional"
+                  : "unidade genérica"}
+              </sub>
+            </div>
+            <div>
+              <sub>Multiplicador: {unidade?.multiplicador}</sub>
+            </div>
+          </ContainerTooltip>
+        );
       },
     },
   };
